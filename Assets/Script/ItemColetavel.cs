1ed1165bc962fb4707a3ae5ad3dc7868203a1ea5
Pablo Souza <150870639+PSdevj@users.noreply.github.com>
2024-11-29 12:22:03 -0300

    Atualização

---
+++ Assets/Script/ItemColetavel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemColetavel : MonoBehaviour
+{
+
+   //Código responsável por coletar e acrescentar os item, além da códição de vitória da segunda cena.
+    public int abacate = 0;
+    public Text abacateText;
+    public ControllGame genJ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ModTexto();
+        genJ = GameObject.FindGameObjectWithTag("GameController").GetComponent<ControllGame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Abacate")
+        {
+            Destroy(collision.gameObject);
+            abacate++;
+            ModTexto();
+            if (abacate == 10)
+            {
+                Debug.Log("Vitória");
+                genJ.VitoriaPlayer();
+            }
+        }
+    }
+
+
+    void ModTexto()
+    {
+        abacateText.text = abacate.ToString() + "x";
+    }
+}
